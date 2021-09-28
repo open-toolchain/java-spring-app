c405d79e8bd44c7329ad8b6c996c22d643de8a1a
anamika.agrawal@ibm.com <anamika.agrawal@ibm.com>
2021-09-28 14:46:14 +0530

    fix health check

--- scripts/health-check.sh
+++ scripts/health-check.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-set -e -o pipefail
-for i in {1..5}
-    do
-        echo "Doing Health check. Attempt Number: ${i}"
-        if [ "$(curl -s -w "%{http_code}\n"  http://${VIRTUAL_SERVER_INSTANCE}:8080/v1/ -o /dev/null)" == "200" ]; then
-            echo "App Health Check passed..."
-            echo "Application URL http://${VIRTUAL_SERVER_INSTANCE}:8080/v1/ "
-            exit 0
-        else
-            sleep 1
-        fi
-    done
-echo "Application Health check failed...."
-exit 1
+# VIRTUAL_SERVER_INSTANCE is the IP of the instnace.
+for j in {1..5}; do
+    echo "Performing health check. Attempt Number: ${j}"
+    if [ "$(curl -s -w "%{http_code}\n"  http://${VIRTUAL_SERVER_INSTANCE}:8080/v1/ -o /dev/null)" == "200" ]; then
+       echo "Application health check passed."
+       exit 0
+    elif [ $j -lt 5 ]; then
+        sleep 1
+    else
+        echo "Application health check failed. Marking the deployment as failed."
+        exit 1
+    fi
+done
